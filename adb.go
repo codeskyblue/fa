580b9e42a7963dbce4531c83fcc73806cef70f00
codeskyblue <codeskyblue@gmail.com>
2018-12-18 22:26:55 +0800

    merge code

--- adb.go
+++ adb.go
@@ -81,6 +81,23 @@ func (conn *AdbConnection) readN(n int) (v string, err error) {
 	return string(buf), nil
 }
 
+// respCheck check OKAY, or FAIL
+func (conn *AdbConnection) respCheck() error {
+	status, err := conn.readN(4)
+	switch status {
+	case _OKAY:
+		return nil
+	case _FAIL:
+		data, err := conn.readString()
+		if err != nil {
+			return err
+		}
+		return errors.New(data)
+	default:
+		return fmt.Errorf("Unknown status: %s, should be OKAY or FAIL", strconv.Quote(stat))
+	}
+}
+
 func (conn *AdbConnection) readString() (string, error) {
 	hexlen, err := conn.readN(4)
 	if err != nil {
@@ -137,6 +154,51 @@ func (c *AdbClient) newConnection() (conn *AdbConnection, err error) {
 	return &AdbConnection{netConn}, nil
 }
 
+func (c *AdbClient) sendTwoWay(data string) (string, error) {
+	if _, err := c.Version(); err != nil {
+		return "", err
+	}
+	conn, err := c.newConnection()
+	if err != nil {
+		return "", err
+	}
+	defer conn.Close()
+	if err := conn.SendPacket(data); err != nil {
+		return "", err
+	}
+	return conn.RecvPacket()
+}
+
+func (c *AdbClient) sendOneWay(data string) error {
+	if _, err := c.Version(); err != nil {
+		return err
+	}
+	conn, err := c.newConnection()
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+	if err := conn.SendPacket(data); err != nil {
+		return err
+	}
+	status, err := conn.readN(4)
+	if err != nil {
+		return err
+	}
+	switch status {
+	case _OKAY:
+		return nil
+	case _FAIL:
+		message, err := conn.readString()
+		if err != nil {
+			return err
+		}
+		return errors.New(message)
+	default:
+		return errors.New("Invalid status: " + status)
+	}
+}
+
 func (c *AdbClient) Version() (string, error) {
 	ver, err := c.rawVersion()
 	if err == nil {
@@ -152,8 +214,79 @@ func (c *AdbClient) rawVersion() (string, error) {
 	if err != nil {
 		return "", err
 	}
+	defer conn.Close()
 	if err := conn.SendPacket("host:version"); err != nil {
 		return "", err
 	}
 	return conn.RecvPacket()
+}
+
+type AdbDevice struct {
+	*AdbClient
+	Serial string
+}
+
+func (c *AdbClient) DeviceWithSerial(serial string) *AdbDevice {
+	return &AdbDevice{
+		AdbClient: c,
+		Serial:    serial,
+	}
+}
+
+func (d *AdbDevice) deviceSelector() string {
+	if d.Serial == "" {
+		return "host:transport-any"
+	}
+	return "host-serial:" + d.Serial
+}
+
+func (d *AdbDevice) transportSelector() string {
+	if d.Serial == "" {
+		return "host:transport-any"
+	}
+	return "host:transport:" + d.Serial
+}
+
+func (d *AdbDevice) check() error {
+	_, err := d.sendTwoWay(d.deviceSelector() + ":features")
+	return err
+}
+
+type TCPCmd struct {
+	Cmd          string
+	NeedResponse bool
+}
+
+func (c *AdbClient) Shell(args ...string) (output string, exitCode int, err error) {
+	conn, err := c.newConnection()
+	if err != nil {
+		return
+	}
+	defer conn.Close()
+	// if err = conn.SendPacket("host:features"); err != nil {
+	// 	return
+	// }
+	// _, err = conn.RecvPacket()
+	// if err != nil {
+	// 	return
+	// }
+	if err = conn.SendPacket("host:transport-any"); err != nil {
+		return
+	}
+	ok, _ := conn.Okay()
+	if !ok {
+		err = fmt.Errorf("shell connection broken")
+	}
+	err = conn.SendPacket("shell:" + shellquote.Join(args...) + " ; echo :$?")
+	if err != nil {
+		return
+	}
+	ok, _ = conn.Okay()
+	if !ok {
+		err = fmt.Errorf("shell connection broken")
+	}
+	buf := bytes.NewBuffer(nil)
+	io.Copy(buf, conn)
+	output = string(buf.Bytes())
+	return
 }
